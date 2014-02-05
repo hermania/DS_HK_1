e6d98a18f009fed655a7f6d0872085db707a225f
Ronald Yu <ronaldyu@Ronalds-MacBook-Pro.local>
2014-02-05 20:16:50 +0800

    All happy

---
+++ lessons/lesson02/__init__.py
@@ -0,0 +1,11 @@
+# matrix multiplication
+"""
+"""
+
+
+def matrixTranspose(matrix):
+     return [ [row[i] for row in matrix] for i in range(len(matrix[0]))]
+
+     def vectorMatrixMultiply(matrix, value):
+    return [x*value for x in matrix]
+def 
